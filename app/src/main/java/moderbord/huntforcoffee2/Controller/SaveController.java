ca9129aff22a6b140ae308226a22e0d46cc754ad
Moderbord <pontande@gmail.com>
2015-12-20 20:39:58 +0100

    Added support for saving multiple Enteties

--- app/src/main/java/moderbord/huntforcoffee2/Controller/SaveController.java
+++ app/src/main/java/moderbord/huntforcoffee2/Controller/SaveController.java
@@ -6,6 +6,8 @@ import android.content.SharedPreferences;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import moderbord.huntforcoffee2.Model.Entity;
+import moderbord.huntforcoffee2.Model.Jheero;
 import moderbord.huntforcoffee2.Model.Player;
 import moderbord.huntforcoffee2.Model.item.Item;
 
@@ -30,16 +32,17 @@ public class SaveController {
         return instance;
     }
 
-    public void savePlayer(Player player){
+    public void saveEntity(Entity entity){
         SharedPreferences sp = c.getSharedPreferences(PLAYER, 0);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString(player.geteName(), player.toJson());
+        editor.putString(entity.geteName(), entity.toJson());
         editor.commit();
     }
 
-    public Player loadPlayer(String playerName){
+    public Entity loadEntity(Entity entity){
+        String entityName = entity.geteName();
         SharedPreferences sp = c.getSharedPreferences(PLAYER, 0);
-        String retrievedFromMemory = sp.getString(playerName, null);
+        String retrievedFromMemory = sp.getString(entityName, null);
         if (retrievedFromMemory == null){
             return null;
         }
@@ -48,7 +51,11 @@ public class SaveController {
         GsonBuilder builder = new GsonBuilder();
         builder.registerTypeAdapter(Item.class, new CustomDeserializer());
         Gson g = builder.create();
-        return g.fromJson(retrievedFromMemory, Player.class);
+        if (entity instanceof Player) {
+            return g.fromJson(retrievedFromMemory, Player.class);
+        } else {
+            return g.fromJson(retrievedFromMemory, Jheero.class);
+        }
     }
 
 }
---
+++ app/src/main/java/moderbord/huntforcoffee2/Model/Jheero.java
@@ -0,0 +1,62 @@
+package moderbord.huntforcoffee2.Model;
+
+import android.util.Log;
+
+import moderbord.huntforcoffee2.Model.item.Armour;
+import moderbord.huntforcoffee2.Model.item.Consumable;
+import moderbord.huntforcoffee2.Model.item.Gear;
+import moderbord.huntforcoffee2.Model.item.Item;
+import moderbord.huntforcoffee2.Model.item.Weapon;
+
+/**
+ * Created by Moderbord on 2015-12-20.
+ */
+public class Jheero extends Entity {
+
+    public Jheero(String eName, String eGender, String eClass, String eRace, String eFaction, int eLevel, int ePhysique, int eIntellect, int eAgility, int eQuickness, int eCharisma, int eLuck, int eLi, int eMaxHealth, int eHealth, int eMaxMana, int eMana, int eMaxFatigue, int eFatigue, int eLu, int eMinLu, int eExperience, int eExpToLvl, Weapon backWep, Weapon mainWep, Weapon offWep, Armour armHead, Armour armShoulders, Armour armChest, Armour armGloves, Armour armLegs, Armour armFeet, Resistance resistance, Inventory inventory) {
+        super(eName, eGender, eClass, eRace, eFaction, eLevel, ePhysique, eIntellect, eAgility, eQuickness, eCharisma, eLuck, eLi, eMaxHealth, eHealth, eMaxMana, eMana, eMaxFatigue, eFatigue, eLu, eMinLu, eExperience, eExpToLvl, backWep, mainWep, offWep, armHead, armShoulders, armChest, armGloves, armLegs, armFeet, resistance, inventory);
+        this.eName = "Jheero";
+        this.eGender = "Male";
+        this.eClass = "Warrior";
+        this.eLevel = 5;
+    }
+
+
+    public void consumeItem(Item i){
+        if(i instanceof Consumable) {
+            Log.d("Jheero", ((Consumable) i).consume());
+            i.subtractItem(1);
+            inventory.notifyDataSetChanged();
+        } else {
+            Log.d("Jheero", "Jheero is hungry!");
+        }
+    }
+
+    public void equipGear(Gear g){
+        if(g instanceof Weapon) {
+
+            if(this.mainWep.getName() != null){     // Save current weapon to inventory
+                this.getInventory().add((this).mainWep);
+            }
+
+            Log.d("Jheero", ((Weapon) g).equip());  // Equip new weapon
+            this.mainWep = (Weapon)g;
+            this.getInventory().remove(g);
+
+        } else if(g instanceof Armour){
+
+            if(this.armChest.getName() != null){    // Save current armour to inventory
+                this.getInventory().add((this).armChest);
+            }
+
+            Log.d("Jheero", ((Armour) g).equip());  // Equip new armour
+            this.armChest = (Armour)g;
+            this.getInventory().remove(g);
+
+        } else {
+            Log.d("Jheero", "That's no weapon");
+        }
+    }
+
+
+}
